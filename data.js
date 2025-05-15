a0ff264857c56c22ec41199e5f29a1c66dc5b443
liezarblade <liezarblade@gmail.com>
2025-05-15 08:15:14 +0530

    Create data.js

---
+++ data.js
@@ -0,0 +1,28 @@
+// data.js
+window.galleryData = [
+  {
+    title: "Movie 1",
+    img: "https://picsum.photos/id/1015/400/200",
+    url: "https://example.com/latest1",
+    section: "latest"
+  },
+  {
+    title: "Movie 2",
+    img: "https://picsum.photos/id/1016/400/200",
+    url: "https://example.com/latest2",
+    section: "latest"
+  },
+  {
+    title: "Recent Add 1",
+    img: "https://picsum.photos/id/1020/400/200",
+    url: "https://example.com/recent1",
+    section: "recent"
+  },
+  {
+    title: "ImMeganLive",
+    img: "8427b060-64b6-45f0-915c-0ff2c5b5dceb.jpg",
+    url: "https://www.skmethung.com",
+    section: "things"
+  },
+  // … add/remove objects here for all 1000+ items …
+];
